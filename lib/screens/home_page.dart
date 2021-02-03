d0488971d0d4dc19af61ed6c6dd5b0619c2bc03c
= <=>
2021-02-03 23:16:43 +0530

    Home Bottom Navigation Added

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,8 +5,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepOrange,
+        selectedItemColor: Colors.white,
+        elevation: 20,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('My Profile'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
