21b1b725f16b1b728b3ca0f49528305466773ee1
rdaftari-unlocked <rdaftari@deqode.com>
2021-02-08 12:24:50 +0530

    .git/config fix

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -7,7 +7,6 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
         color: Colors.deepOrange,
         child: Column(
